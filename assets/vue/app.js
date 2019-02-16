385e4afa1d88da29032150c8cc9c04e79d1a573e
Delphine <veyssiere.d@gmail.com>
2019-02-16 16:38:20 +0100

    displaying vue from js data

---
+++ assets/vue/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+require('../css/app.css');
+require('../css/main.css');
+require('../css/font-awesome.min.css');
+
+
+new Vue({
+    template: '<App/>',
+    components: { App },
+}).$mount('#app');
