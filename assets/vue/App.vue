b70253766959650ef6b18dcc80ff22ada6fc26ea
prideone <stephane.bolu@gmail.com>
2019-02-17 16:30:58 +0100

    add axios and return first response fetched from api

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -1,27 +1,50 @@
 
-
-
 <template>
-    <div class="container">
-        <h1 class="red">H1 : test sur la page</h1>
-        <h2>Affichage des données de voyage</h2>
-        <p>Titre du voyage : {{journey.title}}</p>
-        <p>Description du voyage : {{journey.description}}</p>
-    </div>
+    <ul v-if="journies && journies.length">
+        <li v-for="journey of journies">
+            <p><strong>{{journey.title}}</strong></p>
+            <p>{{journey.description}}</p>
+        </li>
+    </ul>
+
 </template>
 
 
 <script>
+    import axios from 'axios';
+    let config = require('../config/config.json');
+
     export default {
         name: 'app',
         data(){
         	return{
-        		journey:{
-        			title : "périple au Pérou",
-        			description : "voyage de 2 semaine au coeur des cités incas"
-        		}
-
+                loading : false,
+                journies : [],
+                errors : []
         	}
+        },
+        created () {
+            // fetch the data when the view is created and the data is
+            // already being observed
+            this.fetchData()
+        },
+        methods : {
+            fetchData () {
+                this.error = this.post = null;
+                this.loading = true;
+                // replace `getPost` with your data fetching util / API wrapper
+
+                axios.get(config.host+"/api"+config.getJournies)
+                    .then(response => {
+                        // JSON responses are automatically parsed.
+                        console.log(response.data);
+                        this.journies = response.data;
+                    })
+                    .catch(e => {
+                        this.errors.push(e);
+                    });
+
+            }
         }
     }
 </script>
